8a41d6aeb825231e6b9dfe57c341a44df6425014
Radu <radu.rentea@gmail.com>
2022-10-13 19:55:42 +0300

    Remove unnecessary files;Add mouse tracking logic

---
+++ src/components/MouseTracker.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Your mouse is at position: {{ x }}, {{ y }}
+    </div>
+</template>
+
+<script setup>
+import { useMouse } from '../composables/mouse.js';
+
+const { x, y } = useMouse()
+
+</script>
